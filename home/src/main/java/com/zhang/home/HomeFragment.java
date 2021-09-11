4d15d6793201b3150381421a219154c02d5f89a0
大写的旭 <3070448724@qq.com>
2021-09-11 18:43:33 +0800

    three

--- home/src/main/java/com/zhang/home/HomeFragment.java
+++ home/src/main/java/com/zhang/home/HomeFragment.java
@@ -1,17 +1,21 @@
 package com.zhang.home;
 
-import android.os.Bundle;
+import com.zhang.home.mvp.BaseFragment;
 
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-public class HomeFragment extends Fragment {
+public class HomeFragment extends BaseFragment {
+    private static final String TAG = "HomeFragment";
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public int bandLayout() {
+        return R.layout.fragment_home;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
     }
 }
---
+++ home/src/main/java/com/zhang/home/mvp/IFragment.java
@@ -0,0 +1,23 @@
+package com.zhang.home.mvp;
+
+import android.view.View;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.LayoutRes;
+
+/**
+ * @ClassName IFragment
+ * @Description TODO
+ * @Author 张海旭
+ * @Date 2021/9/11 18:09
+ * @Version 1.0
+ * Created by Android Studio.
+ * User: 张海旭
+ */
+public interface IFragment {
+    @LayoutRes
+    int bandLayout();
+    void initView();
+    void initData();
+    public <T extends View> T findViewById(@IdRes int id);
+}
