22db5b6e85aec0f8c67d5881cb565f463b8fcf5c
大写的旭 <3070448724@qq.com>
2021-09-12 19:16:07 +0800

    修复bug

---
+++ mvp_core/src/main/java/com/zhang/mvp_core/Mvp.java
@@ -0,0 +1,13 @@
+package com.zhang.mvp_core;
+
+/**
+ * @ClassName Mvp
+ * @Description TODO
+ * @Author 张海旭
+ * @Date 2021/9/12 19:14
+ * @Version 1.0
+ * Created by Android Studio.
+ * User: 张海旭
+ */
+public class Mvp {
+}
