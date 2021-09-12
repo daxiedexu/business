5067b8be120bd596e78ced0a3faac2786866e113
zhangyitong <2783807474@qq.com>
2021-09-12 19:29:06 +0800

    'SPI实现'

---
+++ testphoto/src/main/java/com/bw/testphoto/Test.java
@@ -0,0 +1,19 @@
+package com.bw.testphoto;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author 张溢通
+ * @Date 2021/9/10 19:16
+ * @Version 1.0
+ * Created by Android Studio.
+ * User: 伊莎贝拉
+ */
+public interface Test {
+    void cs(String uri, Context context, ImageView view);
+
+}
