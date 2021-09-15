b521ccffc8a8c22ca3df8ecc7c6b27331224e180
大写的旭 <3070448724@qq.com>
2021-09-15 10:47:09 +0800

    组件化修复

--- home/src/main/java/com/zhang/home/lazy/BaseFragment.java
+++ home/src/main/java/com/zhang/home/lazy/BaseFragment.java
@@ -1,4 +1,4 @@
-package com.zhang.home.mvp;
+package com.zhang.home.lazy;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,18 +15,19 @@ import org.jetbrains.annotations.NotNull;
  * @ClassName BaseFragment
  * @Description TODO
  * @Author 张海旭
- * @Date 2021/9/11 18:09
+ * @Date 2021/9/15 9:06
  * @Version 1.0
  * Created by Android Studio.
  * User: 张海旭
  */
-public abstract class BaseFragment extends Fragment implements IFragment {
-    private View baseView = null;
+public abstract class BaseFragment extends Fragment {
+    private View baseView;
+    private boolean isLazyLoad;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
-    public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         baseView = inflater.inflate(bindLayout(),container,false);
         return baseView;
     }
@@ -35,11 +36,23 @@ public abstract class BaseFragment extends Fragment implements IFragment {
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initView();
-        initData();
     }
 
     @Override
-    public <T extends View> T findViewById(int id) {
-        return baseView.findViewById(id);
+    public void onActivityCreated(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (!isLazyLoad){
+            loadData();
+        }
+    }
+
+    protected abstract void loadData();
+
+    protected abstract void initView();
+
+    protected abstract int bindLayout();
+
+    public void setLazyLoad(boolean lazyLoad) {
+        isLazyLoad = lazyLoad;
     }
 }
