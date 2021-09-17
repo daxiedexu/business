ef81b0d94ef7784b5e9cc64d9ce317690116448d
大写的旭 <3070448724@qq.com>
2021-09-17 20:42:41 +0800

    four ten

---
+++ home/src/main/java/com/zhang/home/adapter/DiscountAdapter.java
@@ -0,0 +1,38 @@
+package com.zhang.home.adapter;
+
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.viewholder.BaseViewHolder;
+import com.zhang.home.width.MoreMoneyView;
+import com.zhang.home.R;
+import com.zhang.home.entity.DiscountEntity;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * @ClassName DiscountAdapter
+ * @Description TODO
+ * @Author 张海旭
+ * @Date 2021/9/17 16:22
+ * @Version 1.0
+ * Created by Android Studio.
+ * User: 张海旭
+ */
+public class DiscountAdapter extends BaseQuickAdapter<DiscountEntity, BaseViewHolder> {
+    public DiscountAdapter(@Nullable List<DiscountEntity> data) {
+        super(R.layout.discount_item, data);
+    }
+
+    @Override
+    protected void convert(@NotNull BaseViewHolder baseViewHolder, DiscountEntity discountEntity) {
+        ImageView imageView = baseViewHolder.getView(R.id.discount_image);
+        MoreMoneyView moreMoneyView = baseViewHolder.findView(R.id.discount_more);
+        Glide.with(getContext()).load(discountEntity.getDiscountUrl()).into(imageView);
+        moreMoneyView.bringToFront();
+    }
+}
--- home/src/main/java/com/zhang/home/width/MoreMoneyView.java
+++ home/src/main/java/com/zhang/home/width/MoreMoneyView.java
@@ -20,6 +20,7 @@ public class MoreMoneyView extends androidx.appcompat.widget.AppCompatTextView {
     private Paint mPaint;
     private Paint textPaint;
     private Path mPath;
+    private Path textPath;
     public MoreMoneyView(Context context) {
         super(context);
         init();
@@ -38,21 +39,26 @@ public class MoreMoneyView extends androidx.appcompat.widget.AppCompatTextView {
     private void init() {
         mPaint = new Paint();
         textPaint = new Paint();
-        textPaint.setTextSize(36);
-        textPaint.setColor(Color.BLACK);
+        textPaint.setTextSize(30);
+        textPaint.setColor(Color.WHITE);
+        textPaint.setStyle(Paint.Style.FILL);
         textPaint.setTextAlign(Paint.Align.CENTER);
-        textPaint.setStrokeWidth(10);
+        textPaint.setStrokeWidth(12);
         mPath = new Path();
+        textPath = new Path();
         mPaint.setColor(Color.RED);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        mPath.moveTo(80,200);
-        mPath.lineTo(200,200);
-        mPath.lineTo(200,300);
+        mPath.moveTo(80,0);
+        mPath.lineTo(200,0);
+        mPath.lineTo(200,100);
         mPath.close();
+        textPath.lineTo(200,80);
+        textPath.lineTo(200,130);
         canvas.drawPath(mPath,mPaint);
+        canvas.drawTextOnPath("折扣",textPath,20,-17,textPaint);
     }
 }
